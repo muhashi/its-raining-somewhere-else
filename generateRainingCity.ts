fd31c6ccf8656a5d5646b7e11a07ab23c52b941e
muhashi <105213357+muhashi@users.noreply.github.com>
2023-06-18 15:57:41 +0000

    Add backend for generating raining city

---
+++ generateRainingCity.ts
@@ -0,0 +1,60 @@
+import cities from 'cities.json' assert { type: "json" };
+import arrayShuffle from 'array-shuffle';
+import {setTimeout} from 'node:timers/promises';
+import got from 'got';
+
+const MAX_NUMBER_CITIES = 400;
+const delayMs = 200;
+
+type CityData = {
+    lat: string,
+    lng: string,
+    name: string,
+    country: string,
+}
+
+type RainData = {
+    hourly: {
+        time: string[],
+        rain: number[],
+    },
+}
+
+// rounded down to nearest hour, slices of seconds and ms
+function getCurrentISOTimeString() {
+    const d = new Date();
+    d.setMinutes(0, 0);
+    return d.toISOString().slice(0, -8);
+}
+
+export async function getRainingCity(): Promise<CityData | null> {
+    const randomCities = arrayShuffle(cities as readonly CityData[]);
+    const maxLength = Math.min(MAX_NUMBER_CITIES, randomCities.length);
+
+    for (let i = 0; i < maxLength; i++) {
+        const {lat, lng} = randomCities[i];
+
+        if (!lat || !lng) continue;
+        
+        const url = `https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lng}&hourly=rain`;
+
+        const res: RainData = await got(url).json();
+        if (!res || !res?.hourly || !res?.hourly?.rain) continue;
+        const currentTime = getCurrentISOTimeString();
+        const {time, rain} = res.hourly;
+
+        const index = time.findIndex((timeString) => timeString === currentTime);
+
+        if (index === -1 || index >= rain.length) continue;
+
+        const currentRain = rain[index];
+
+        if (currentRain > 0) {
+            return randomCities[i];
+        }
+
+        await setTimeout(delayMs);
+    }
+
+    return null;
+}
---
+++ index.ts
@@ -0,0 +1,18 @@
+import express from 'express';
+import {getRainingCity} from './generateRainingCity.js';
+import memoize from 'memoizee';
+
+const app = express();
+const port = 3000;
+
+const memoizedGetRainingCity = memoize(getRainingCity, {promise: true, maxAge: 60 * 60 * 1000});
+
+app.get('/', async (req, res) => {
+  const countryData = await memoizedGetRainingCity();
+
+  res.json(countryData);
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
