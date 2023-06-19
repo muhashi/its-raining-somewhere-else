21deb91ffe6d7916efa44bdbb47d01d6b3d66db2
muhashi <105213357+muhashi@users.noreply.github.com>
2023-06-19 15:52:18 +1000

    Add CORS for client site

--- server/index.ts
+++ server/index.ts
@@ -1,9 +1,17 @@
+import cors from 'cors';
+import 'dotenv/config';
 import express from 'express';
 import memoize from 'memoizee';
 import { getRainingCity } from './generateRainingCity.js';
 
 const app = express();
 const port = process.env.PORT ? parseInt(process.env.PORT) : 3000;
+
+console.log(process.env.CLIENT_URL)
+
+app.use(cors({
+  origin: process.env.CLIENT_URL ?? "*",
+}));
 
 const memoizedGetRainingCity = memoize(getRainingCity, {promise: true, maxAge: 60 * 60 * 1000});
 
